5e37c31a3138964856048bfbeaad464287617efb
Fer Perales <me@ferperales.net>
2014-01-19 18:06:30 -0600

    Add rails application skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FullcalendarAndRailsExample::Application.config.secret_key_base = '9609800e7bff0bbf1f542c6e3136f1bf359f4b35ecad7e4ca10a8955110e4e822f951163a2eceaa2977191b20e8d01a8152e60c209c8fc740c06f03b22a68776'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FullcalendarAndRailsExample::Application.config.session_store :cookie_store, key: '_fullcalendar_and_rails_example_session'
